23eb78022346762b126cd12bfaf2354e0efd49f7
RMSuyama <rafaelmoreirasuyama@gmail.com>
2022-11-25 21:58:38 -0300

    alterei umas coisinhas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './assets/logo.svg';
 import logo2 from './assets/logo2.png';
 import fundo from './assets/FUNDO.png'
-import sLetter from './assets/S.png'
 import rLetter from './assets/R.png'
 import './App.css';
+import whats from './assets/whats.png'
 
 function App() {
 
@@ -25,20 +25,20 @@ function App() {
         </div>
         <div className='floatingLettersContainer'>
             <img className='rContainer' src={rLetter} />
-            <img className='sContainer' src={sLetter} />
+            {/* <img className='sContainer' src={sLetter} /> */}
           <a
           className="App-link"
           href="https://api.whatsapp.com/send?phone=5513996267989"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h1> Whatsapp me</h1>
+          <img className='whats' src={whats} />
         </a>
         </div>
       </div>
         {/* ////aqui seria a div do linkContainer */}
-      <div className='linkContainer'>
-      </div>
+      {/* <div className='linkContainer'>
+      </div> */}
     </div>
   );
 }
