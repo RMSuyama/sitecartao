68c1968446e5dd25cabcc92ea03e688507ad51d8
RMSuyama <rafaelmoreirasuyama@gmail.com>
2022-11-29 13:11:26 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import logo from './assets/logo.svg';
-import logo2 from './assets/logo2.png';
 import fundo from './assets/FUNDO.png'
 import rLetter from './assets/R.png'
-import './App.css';
 import whats from './assets/whats.png'
+import insta from './assets/insta.png'
+import React from 'react'
+import './App.css'
+
+
 
 function App() {
 
-  // const oi = () => {
-  //   console.log(' oioooooooooo ')
-  // }
-
-  // oi();
-
   return (
-    // nessa seção serão colocados aspectos soltos
+    <>
+    
+      <div className="Title">
+        <img className='rContainer' src={rLetter} />
+      </div>
 
+      <div className="App">
 
-    <div className="App">
-
-      <div className='fundoContainer'>
-        <div className='imgContainer'>
-          <img className='fundoBg' src={fundo} />
-        </div>
-        <div className='floatingLettersContainer'>
-            <img className='rContainer' src={rLetter} />
-            {/* <img className='sContainer' src={sLetter} /> */}
+        <div className='fundoContainer'>
+          <div className='imgContainer'>
+            <img className='fundoBg' src={fundo} />
+          </div>
+          <div className='floatingLettersContainer'>
             <a
               className="App-link"
               href="https://api.whatsapp.com/send?phone=5513996267989"
@@ -34,12 +31,18 @@ function App() {
             >
               <img className='whats' src={whats} />
             </a>
+            <a
+              className="App-link"
+              href="https://api.whatsapp.com/send?phone=5513996267989"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className='insta' src={insta} />
+            </a>          
           </div>
         </div>
-        {/* ////aqui seria a div do linkContainer */}
-      {/* <div className='linkContainer'>
-      </div> */}
-    </div>
+      </div></>
+
   );
 }
 
