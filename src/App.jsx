4749dcf18353067d4cddfc7f379efbf837e5972e
Luís Fernando Arnoni Fritzen <spd508@einstein.br>
2022-12-01 11:06:16 -0300

    style: formats components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import React from 'react'
 import './App.css'
-import { ContactIcons } from './components/ContactIcons';
-import Body from './components/layout/Body';
-import Footer from './components/layout/Footer';
+import { ContactIcons } from './components/ContactIcons'
+import Body from './components/layout/Body'
+import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
 
-
-
 const App = () => {
-
-  return (
-    <>  
-      <div className="App">
-        <ContactIcons />
-        <div className='header'>
-          <Header/>
-        </div>
-        <div className='main'>
-        <main/>
-        <Body/>
-        </div>
-        <div className='aside'>
-        
-        </div>
-        <div className='footer'>
-        <Footer/>
-        </div>
-        </div></>
-  );
+    return (
+        <>
+            <div className="App">
+                <ContactIcons />
+                <div className="header">
+                    <Header />
+                </div>
+                <div className="main">
+                    <main />
+                    <Body />
+                </div>
+                <div className="aside"></div>
+                <div className="footer">
+                    <Footer />
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,13 @@
 import './Footer.css'
 
 function Footer() {
-
-    return(
+    return (
         <div className="tfooter">
-               <p>Rafael Suyama</p>
-               <p>OAB nº 484.261</p>
-               <p>Advocacia e Consultoria Jurídica Previdenciária</p>
+            <p>Rafael Suyama</p>
+            <p>OAB nº 484.261</p>
+            <p>Advocacia e Consultoria Jurídica Previdenciária</p>
         </div>
     )
-
 }
 
 export default Footer
