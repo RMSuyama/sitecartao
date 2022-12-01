1663f910ffd8d4420c1889288f25304c921ea099
Luís Fernando Arnoni Fritzen <spd508@einstein.br>
2022-12-01 10:56:38 -0300

    style(header): remove unused comment

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,33 +2,13 @@ import './Header.css'
 import rLetter from '../../assets/R.png'
 
 function Header() {
-
-    return(
+    return (
         <div className="theader">
 
         <img className='logo' src={rLetter} />
-        
-        {/* <div className='linkRow'>
-              <a
-              className="Whatsapp"
-              href="https://api.whatsapp.com/send?phone=5513996267989"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img className='whats' src={whats} />
-            </a>
-            <a
-              className="Instagram"
-              href="https://www.instagram.com/direct/inbox/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img className='insta' src={insta} />
-            </a>          
-        </div> */}
-      </div>
-    )
 
+        </div>
+    )
 }
 
 export default Header
