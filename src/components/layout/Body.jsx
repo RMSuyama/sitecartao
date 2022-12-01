b985e8491ee8057a26d2391d12c7ac3bd6c18bd0
RMSuyama <rafaelmoreirasuyama@gmail.com>
2022-12-01 02:21:07 -0300

    troquei os textos

--- src/components/layout/Body.jsx
+++ src/components/layout/Body.jsx
@@ -4,15 +4,17 @@ function Body() {
 
     return(
         <div className="tbody">
-        <h1> A toca do Direito Penal </h1>
-        <p>Aperte o freio e se prepare para uma jornada mágica. 
-            O livro apresenta a minha história, a trajetória de um menino curioso que segue um Coelho Branco de colete e relógio, mergulhando, sem pensar, na toca do direito penal. 
-            O protagonista é projetado para um novo mundo de rigor inexorável aos olhos de estranhos, repleto de leis, técnicas hermenêuticas, teses absolutórias, dosimetrias de pena e estratégias processuais</p>
-        <p>No Universo do Direito Penal., o nosso herói, que sou eu, vive aventuras e é confrontado com o absurdo, o impossível, o inadmissível, o inaceitável, o intolerável, questionando, com isso, tudo o que aprendera até então. Durante a trajetória, por diversas vezes me perguntei se não sou eu apenas um observador em um universo repleto de julgamentos sem sentido, com condenações à pena de morte por uma Rainha de Copas, tirana que tem por costume mandar cortar a cabeça de todos que a incomodam.</p>    
-        <p>Confesso que todos esses pensamentos tomaram minha mente e me devastaram durante o processo, o que me levou a reflexão acerca do nosso papel no que diz respeito a tudo isso. Ante essas reflexões, apresento a você, caro leitor o seguinte questionamento:</p>
-        <p>“Faço eu, parte, tão somente, de uma história idiota, contada por um tolo, cheia de som e de fúria, que no final das contas significa, de forma absoluta, um grandissíssimo nada? Ou serei eu acordado, na iminência de ser levado pelos soldados de copas, da nossa aventura pelo sistema penal, acordado desse pesadelo que deve ser combatido, devendo com isso, toda injustiça e opressão ser fortemente rechaçada e combatida pela perícia e maestria das práticas processuais do direito penal?”. </p>
-        <p>Acredito que a resposta para esse dilema é um tanto quanto óbvia, mas não o cercarei de obviedades e partirei para o início dessa, terrível e empolgante, jornada. Para acompanhar essa grande aventura épica jurídica venha comigo nessa revisão penal e processual penal</p>
-        <p>Autor: Desconhecido</p>  
+        <h1> O que é o Direito Previdenciário? </h1>
+        <p>O direito previdenciário é uma área do direito público que estuda a regulamentação da Seguridade Social. Seu objetivo é disciplinar a Previdência Social, definindo regras sobre o recolhimento de contribuições sociais e normas para concessão de benefícios previdenciários, como aposentadoria, auxílio-doença e pensão.
+O direito previdenciário garante a dignidade das pessoas conforme previsto nos artigos 6º e 201 da Constituição Federal. Afinal, é dever do Estado amparar os beneficiários, segurados e dependentes, de acordo com o que está previsto na legislação previdenciária. </p>
+        <p>Pedido de aposentadorias;</p>
+        <p>Requisição de benefícios por incapacidade;</p>
+        <p>Recebimento de pensão por morte;</p>
+        <p>Pgamento do salário-maternidade; </p>
+        <p>Restituição de INSS e contribuições em atraso;</p>
+        <p>Verificação de fraudes previdenciárias.</p>
+
+        <p>Se você se interessou, entre em contato pelos links na tela!</p>
         </div>
     )
 
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
 
     return(
         <div className="tfooter">
-               <p>"Um livro é a prova de que os homens são capazes de fazer magia. Palavra puxa palavra, uma ideia traz outra, e assim se faz um livro, um governo, ou uma revolução, alguns dizem que assim é que a natureza compôs as suas espécies. Um livro é como uma janela.”</p>
+               <p>Rafael Suyama</p>
+               <p>OAB nº 484.261</p>
+               <p>Advocacia e Consultoria Jurídica Previdenciária</p>
         </div>
     )
 
