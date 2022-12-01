ec6c3cd8f544f2f4fe70bce2ec02fbfcac6c60d9
Luís Fernando Arnoni Fritzen <spd508@einstein.br>
2022-12-01 10:58:15 -0300

    style: format header spaces

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,11 +2,11 @@ import './Header.css'
 import rLetter from '../../assets/R.png'
 
 function Header() {
-    return (
-        <div className="theader">
-            <img className="logo" src={rLetter} alt="logo" />
-        </div>
-    )
+  return (
+    <div className="theader">
+      <img className="logo" src={rLetter} alt="logo" />
+    </div>
+  )
 }
 
 export default Header
